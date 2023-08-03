12364b41a8a2f2f2f6c56eff22db7b54a085e2a0
JoaquinJanco <141292297+JoaquinJanco@users.noreply.github.com>
2023-08-02 23:26:35 -0300

    Mercado Liebre

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log("Servidor abierto en el puerto "+port);
+  });
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "home.html"));
+});
