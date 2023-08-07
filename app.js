92b01f34a06c0d146bb5d0cd09d9ff7d78068ec6
JoaquinJanco <141292297+JoaquinJanco@users.noreply.github.com>
2023-08-06 23:26:47 -0300

    Ultimas actualizaciones

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.listen(port, () => {
     console.log("Servidor abierto en el puerto "+port);
   });
 
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "register.html"));
 });
